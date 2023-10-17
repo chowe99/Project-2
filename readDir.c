e34fa16b0401d9b9427be3c3508012979d19c9cd
chowe99 <chowewoow@gmail.com>
2023-10-17 14:43:10 +0800

    read_dir function updated

--- readDir.c
+++ readDir.c
@@ -1,1 +1,31 @@
 #include "mysync.h"
+
+void read_dir(char *dirname) {
+    DIR *dirp;
+    struct dirent *dp;
+
+    dirp = opendir(dirname);
+    if (dirp == NULL) {
+        perror(dirname);
+        exit(EXIT_FAILURE);
+    }
+
+    while ( (dp = readdir(dirp)) != NULL) 
+    {
+        if (i_index > 0 && is_match(dp->d_name, i, i_index)) {
+            continue;
+        }
+        if (o_index > 0 && !is_match(dp->d_name, o, o_index)) {
+            continue;
+        }
+
+        struct stat info;
+        char pathname[MAXPATHLEN];
+        sprintf(pathname, "%s/%s", dirname, dp->d_name); 
+        if (stat(pathname, &info) != 0) {
+            perror(pathname);
+            exit(EXIT_FAILURE);
+        }
+        printf("modified: %ld\n", info.st_mtim.tv_sec);
+    }
+}
