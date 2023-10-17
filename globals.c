9cb5d1925d8b7fa2eb4ad6b805939aba9b78a64f
chowe99 <chowewoow@gmail.com>
2023-10-17 23:16:32 +0800

    formatted layout

--- globals.c
+++ globals.c
@@ -2,17 +2,16 @@
 
 //to be filled in more
 //allocating space and initial values for global variables 
-bool a = false;
-bool n = false;
-bool p = false;
-bool r = false;
-bool v = false;
-char **o = NULL;
-size_t o_index = 0;
-char **i = NULL;
-size_t i_index = 0;
-HASHTABLE *file_list;
-
+bool        a = false;
+bool        n = false;
+bool        p = false;
+bool        r = false;
+bool        v = false;
+char        **o = NULL;
+size_t      o_index = 0;
+char        **i = NULL;
+size_t      i_index = 0;
+HASHTABLE   *file_list;
 
 /* checks command line options and saves them to corresponding
  * global variables, returns number of options found.
