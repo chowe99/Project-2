cf49200151a27b8ecff217c2478f165ec9d8c113
chowe99 <chowewoow@gmail.com>
2023-10-17 14:42:52 +0800

    is_match function implemented

--- patterns.c
+++ patterns.c
@@ -1,1 +1,18 @@
 #include "mysync.h"
+
+bool is_match(char *filename, char *arr[], size_t num_items) {
+    for (size_t i = 0; i<num_items; i++) {
+        regex_t reegex;
+        if (regcomp(&reegex, glob2regex(arr[i]), 0) != 0) {
+            perror(glob2regex(arr[i]));
+            exit(EXIT_FAILURE);
+        }
+        if (regexec(&reegex, filename, 0, NULL, 0) == 0)
+        {
+            printf("found match\n");
+            return true;
+        }
+    }
+    printf("no match\n");
+    return false;
+}
