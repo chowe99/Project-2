b1d05f6c3d3ef468872a7fee87b58d22b79601e4
chowe99 <chowewoow@gmail.com>
2023-10-20 13:40:40 +0800

    initialisers formatted for readability

--- globals.c
+++ globals.c
@@ -2,18 +2,18 @@
 
 //to be filled in more
 //allocating space and initial values for global variables 
-bool        a = false;
-bool        n = false;
-bool        p = false;
-bool        r = false;
-bool        v = false;
-char        **o = NULL;
-size_t      o_index = 0;
-char        **i = NULL;
-size_t      i_index = 0;
-int         nfiles =0;
-char**      filenames = NULL;
-HASHTABLE   *file_list = NULL;
+bool        a           = false;
+bool        n           = false;
+bool        p           = false;
+bool        r           = false;
+bool        v           = false;
+char        **o         = NULL;
+size_t      o_index     = 0;
+char        **i         = NULL;
+size_t      i_index     = 0;
+int         nfiles      = 0;
+char**      filenames   = NULL;
+HASHTABLE   *file_list  = NULL;
 
 /** Gets the command line arguments and saves them to global 
  *  variables.
