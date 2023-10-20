a2b0bf5a552a5a2409efc360a09eef03a15917e5
chowe99 <chowewoow@gmail.com>
2023-10-20 15:22:46 +0800

    list comments and file reformatted

--- list.c
+++ list.c
@@ -1,9 +1,7 @@
 #include "mysync.h"
 
-
-/**
- * Creates a new ,empty list
- * \returns: NULL pointer
+/** Creates a new ,empty list
+ * \return NULL pointer
 */
 LIST *list_new(void)
 {
@@ -12,30 +10,29 @@ LIST *list_new(void)
 
 /**
  * Determines if a given file name exists in the list 
- * \param: LIST list: the list to be searched
- * \param: wanted: pointer to the filename to be searched for
- * \return: true if the file was found, false otherwise
+ * \param LIST list: the list to be searched
+ * \param wanted: pointer to the filename to be searched for
+ * \return true if the file was found, false otherwise
 */
 bool list_find(LIST *list, char *wanted)
 {
     while(list != NULL) {
-	if(strcmp(list->file_name, wanted) == 0) {
-	    return true;
-	}
-	list	= list->next;
+        if(strcmp(list->file_name, wanted) == 0) {
+            return true;
+        }
+        list	= list->next;
     }
     return false;
 }
 
 /**
  * Allocates space for a new list item
- * \param: filename: filename of the file to be added
- * \param: mtime: modification time of the file
- * \param: permissions: permissions of the file
- * \param: dirname: Top level directory of the file 
- * \return: returns the new list item 
+ * \param filename: filename of the file to be added
+ * \param mtime: modification time of the file
+ * \param permissions: permissions of the file
+ * \param dirname: Top level directory of the file 
+ * \return the new list item 
 */
- 
 LIST *list_new_item(char* filename, time_t mtime, mode_t permissions, char* dirname)
 {
     LIST *new       = calloc(1, sizeof(LIST));
@@ -49,78 +46,75 @@ LIST *list_new_item(char* filename, time_t mtime, mode_t permissions, char* dirn
     return new;
 }
 
-/**
- * Adds a new list item to a given list 
- * \param: filename: filename of the file to be added
- * \param: list: the list to add the item too
- * \param: mtime: modification time of the file
- * \param: permissions: permissions of the file
- * \param: dirname: Top level directory of the file 
- * \return: returns the updated list  
+/** Adds a new list item to a given list 
+ *
+ * \param filename filename of the file to be added
+ * \param list the list to add the item too
+ * \param mtime modification time of the file
+ * \param permissions permissions of the file
+ * \param dirname Top level directory of the file 
+ * \return the updated list  
 */
 LIST *list_add(LIST *list, char* filename, time_t mtime, mode_t permissions, char* dirname)
 {
 
-        LIST *new  = list_new_item(filename, mtime, permissions,dirname );
-        new->next   = list;
-        list_print(new);
-        return new;
+    LIST *new  = list_new_item(filename, mtime, permissions,dirname );
+    new->next   = list;
+    list_print(new);
+    return new;
 
 }
 
-/**
- * Print each item of the given list to stdout
+/** Print each item of the given list to stdout
+ *
+ * \param list the list to print
 */
 void list_print(LIST *list)
 {
     if(list != NULL) {
         while(list != NULL) {
-	    printf("%s", list->file_name);
-	    if(list->next != NULL) {
-	        printf(" -> ");
+            printf("%s", list->file_name);
+            if(list->next != NULL) {
+                printf(" -> ");
             }
-	    list	= list->next;
+            list	= list->next;
         }
-	printf("\n");
+        printf("\n");
     }
 }
 
-/**
- * Adds a new filename to the array of names that have been added to the hashtable 
- * \param: filename to be added
+/** Adds a new filename to the array of names that have 
+ *  been added to the hashtable 
+ *
+ *  \param filename the file to be added
 */
 void arrayAdd(char *filename) {
-        char* newFilename = malloc(MAXPATHLEN);
-        if (newFilename == NULL) {
-            fprintf(stderr, "Memory allocation error\n");
-            exit(EXIT_FAILURE);
-        }
-        // Copy the filename to the allocated memory
-        strncpy(newFilename, filename, MAXPATHLEN - 1);
-        newFilename[MAXPATHLEN - 1] = '\0'; 
-
-        // Resize the array to accommodate the new filename
-        filenames = realloc(filenames, nfiles * sizeof(char*));
-        if (filenames == NULL) {
-            fprintf(stderr, "Memory reallocation error\n");
-            exit(EXIT_FAILURE);
-        }
-        // Add the new filename to the array
-        filenames[nfiles - 1] = newFilename;
+    char* newFilename = malloc(MAXPATHLEN);
+    if (newFilename == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
     }
-/**
- * Prints all elements of the array to stdout
+    // Copy the filename to the allocated memory
+    strncpy(newFilename, filename, MAXPATHLEN - 1);
+    newFilename[MAXPATHLEN - 1] = '\0'; 
+
+    // Resize the array to accommodate the new filename
+    filenames = realloc(filenames, nfiles * sizeof(char*));
+    if (filenames == NULL) {
+        fprintf(stderr, "Memory reallocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    // Add the new filename to the array
+    filenames[nfiles - 1] = newFilename;
+}
+
+/** Prints all elements of the array to stdout 
 */
 void printArray() {
-            for (int i = 0; i < nfiles; ++i) {
-            printf("File %d: %s \n", i + 1, filenames[i]);
-            LIST *current = file_list[hash_string(filenames[i]) % HASHTABLE_SIZE];
-            printf("Full path before syncing: %s/%s\n", current->dir_name, current->file_name);
-        }
+    for (int i = 0; i < nfiles; ++i) {
+        printf("File %d: %s \n", i + 1, filenames[i]);
+        LIST *current = file_list[hash_string(filenames[i]) % HASHTABLE_SIZE];
+        printf("Full path before syncing: %s/%s\n", current->dir_name, current->file_name);
     }
+}
 
-
-
-    
-
-    
