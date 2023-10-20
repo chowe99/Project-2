f0e726d1058c395bb476b71386b33970f33e8f2b
chowe99 <chowewoow@gmail.com>
2023-10-20 14:06:50 +0800

    modified recursive shelltest

--- shelltest_r.sh
+++ shelltest_r.sh
@@ -11,6 +11,9 @@ mkdir dir1/dir2
 mkdir dir1/dir3
 mkdir dir1/dir3/dir3
 mkdir dir1/dir3/dir3/dir3
+mkdir dir1/dir10
+mkdir dir1/dir10/dir10
+echo "success" > dir1/dir10/dir10/duptest
 mkdir dir1/dir4
 mkdir dir1/dir4/dir5
 mkdir dir1/dir4/dir5/dir6
@@ -36,8 +39,10 @@ printf "Output should be\nNEWEST\nNEWEST\ngot:\n"
 cat dir1/file3
 cat dir1/dir2/file3
 printf "output should be 'nested_file': "
-cat dir1/dir4/dir5/dir6/nested
-printf "there should be no dir3's\n"
+cat dir1/dir2/dir4/dir5/dir6/nested
+printf "none of the dir3s should be synced\n"
+printf "output should be 'success': "
+cat dir1/dir2/dir10/dir10/duptest
 
 #
 rm -rf dir1
