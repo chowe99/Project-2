f016d5bdef6887d376fc291e0654c32d1c58b4ee
chowe99 <chowewoow@gmail.com>
2023-10-20 12:39:43 +0800

    patterns documentation added

--- patterns.c
+++ patterns.c
@@ -1,5 +1,12 @@
 #include "mysync.h"
 
+/** Checks if filename matches regex.
+ *
+ * \param filename The file to be tested
+ * \param arr The regex's to check
+ * \param num_items The number of regex's to check
+ * \return true if it matches else false
+*/
 bool is_match(char *filename, char *arr[], size_t num_items) {
     for (size_t i = 0; i<num_items; i++) {
         regex_t reegex;
