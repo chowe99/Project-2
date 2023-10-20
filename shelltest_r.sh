babbddcbf63c860c47fe58d903fc092fc91e265b
chowe99 <chowewoow@gmail.com>
2023-10-20 13:57:41 +0800

    recursive shelltest created

---
+++ shelltest_r.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+rm -rf dir1
+#
+mkdir dir1
+date > dir1/file1
+cal  > dir1/file2
+echo "new" > dir1/file3
+#
+sleep 1
+mkdir dir1/dir2
+mkdir dir1/dir3
+mkdir dir1/dir3/dir3
+mkdir dir1/dir3/dir3/dir3
+mkdir dir1/dir4
+mkdir dir1/dir4/dir5
+mkdir dir1/dir4/dir5/dir6
+echo "nested_file" > dir1/dir4/dir5/dir6/nested
+# ls   > dir1/dir2/file3
+echo "NEWEST" > dir1/dir2/file3
+#
+echo "before syncing:"
+tree dir1
+tree dir1/dir2
+echo "____________________________"
+#
+./mysync -v -r dir1 dir1/dir2
+echo "after syncing:"
+tree dir1
+tree dir1/dir2
+printf "\n"
+echo "two datetimes should exist:"
+cat dir1/file1
+cat dir1/dir2/file1
+printf "\n"
+printf "Output should be\nNEWEST\nNEWEST\ngot:\n"
+cat dir1/file3
+cat dir1/dir2/file3
+printf "output should be 'nested_file': "
+cat dir1/dir4/dir5/dir6/nested
+printf "there should be no dir3's\n"
+
+#
+rm -rf dir1
