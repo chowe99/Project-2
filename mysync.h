756836659f97ff44eff83eddee126f11927b1762
RhiannaHepburn <rmhepburn04@gmail.com>
2023-10-20 13:04:42 +0800

    mysync update

--- mysync.h
+++ mysync.h
@@ -24,9 +24,7 @@
     exit(EXIT_FAILURE);}
 
 //GLOBAL VARIABLES
-extern int      ndirs;
 extern int      nfiles;
-extern char     *argv0; //program name
 extern bool     verbose; //true iff -v or -n provided 
 extern bool     a;
 extern bool     n;
@@ -37,10 +35,8 @@ extern char     **o;
 extern size_t   o_index;
 extern char     **i;
 extern size_t   i_index;
-extern char     **files_to_sync;
-//FUNCTIONS FROM ..other c files defined 
-//FROM list:
-//  OUR SIMPLE LIST DATATYPE - A DATA ITEM, AND A POINTER TO ANOTHER LIST
+
+
 typedef struct _list {
     char            *file_name; //filename
     char            *dir_name;
@@ -49,70 +45,100 @@ typedef struct _list {
     struct _list    *next;
 } LIST;
 
- typedef struct _file {
-     char        *filename;
-     struct _file   *next;
- } FILEITEM;
-
-
-
 extern char** filenames;
+
+//FUNCTIONS IN GLOB2REGEX.C
+extern char *glob2regex(char *glob);
+extern bool is_match(char *filename, char *arr[], size_t num_items);
+
+//FUNCTIONS IN HASHTABLE.C
+extern uint32_t hash_string(char *string);
+extern HASHTABLE *hashtable_new(void);
+extern void hashtable_add(HASHTABLE *hashtable, char *filename, time_t mtime, mode_t permissions, char* dirname);
+extern bool hashtable_find(HASHTABLE *, char *filename);
+
+//FUNCTIONS IN LIST.C
+extern LIST *list_new(void);
+extern bool list_find (LIST *list, char *wanted);
+extern bool dir_find(LIST *list, char *wanted);
+extern LIST *list_new_item(char* filename, time_t mtime, mode_t permissions, char* dirname);
+extern LIST *list_add(LIST *list, char* filename, time_t mtime, mode_t permissions, char* dirname);
+extern void list_print(LIST *list);
 extern void arrayAdd(char *filename);
 extern void printArray();
+
+//FUNCTIONS IN READDIR.C
+
+
+
+//FUNCTIONS IN FILES.C
+extern void copy_text_file(char destination[], char source[]);
+extern void print_permissions(mode_t mode);
+extern void setPermissions(char *source, char *dest);
+extern void setModTime(char *source, char *dest);
+
+
+//FUNCTIONS IN GLOBALS.C
+extern int save_args(int argc, char *argv[]);
+
 
 
 //  THESE FUNCTIONS ARE DECLARED HERE, AND DEFINED IN list.c :
 
 //  'CREATE' A NEW, EMPTY LIST
-extern LIST	    *list_new(void);
-extern LIST         *list_new_item(char* filename, time_t mtime, mode_t permissions, char* dirname);
+
+
 
 //  ADD A NEW (STRING) ITEM TO AN EXISTING LIST
-extern LIST	    *list_add(LIST *list, char* filename, time_t mtime, mode_t permissions, char* dirname);
-extern void copy_text_file(char destination[], char source[]);
-extern void setModTime(char *source, char *dest);
+
+
+
 
 //  DETERMINE IF A REQUIRED ITEM (A STRING) IS STORED IN A GIVEN LIST
-extern bool	    list_find (LIST *list, char *wanted);
+
 
 //  PRINT EACH ITEM (A STRING) IN A GIVEN LIST TO stdout
-extern void	    list_print(LIST *list);
+
 extern int          compare_mtime_descending(const void *v1, const void *v2);
 //FROM hashtable
 //  WE DEFINE A HASHTABLE AS A (WILL BE, DYNAMICALLY ALLOCATED) ARRAY OF LISTs
 typedef	LIST        *HASHTABLE;
 extern HASHTABLE    *file_list;
 //  THESE FUNCTIONS ARE DECLARED HERE, AND DEFINED IN hashtable.c :
-extern uint32_t     hash_string(char *string);
+
 //  ALLOCATE SPACE FOR A NEW HASHTABLE (AND ARRAY OF LISTS)
-extern HASHTABLE    *hashtable_new(void);
+
 //  ADD A NEW STRING TO A GIVEN HASHTABLE
-extern void	    hashtable_add(HASHTABLE *hashtable, char *filename, time_t mtime, mode_t permissions, char* dirname);
+
 
 //  DETERMINE IF A REQUIRED STRING ALREADY EXISTS IN A GIVEN HASHTABLE
-extern bool         hashtable_find(HASHTABLE *, char *filename);
+
 
 //EXTERNAL FUNCTIONS
 extern char         *strdup(const char *);
 
 //FUNCTIONS FROM globals.c
-extern int          save_args(int argc, char *argv[]);
+
 
 //FUNCTIONS FROM readDir.c
-extern int          read_dir(HASHTABLE *hashtable, char *dirname, char *parentdirs);
-extern void         sync_directories(HASHTABLE *hashtable, char *dirname); 
+extern int read_dir(HASHTABLE *hashtable, char *dirname, char *parentdirs);
+extern void sync_directories(HASHTABLE *hashtable, char *dirname); 
 extern void  printDir(char *dirname);
+extern void add_missing_dirs(const char *subdirectories, const char *parentdir);
+extern void process_path(char *path, char *dirname);
+
+
 //FUNCTIONS FROM files.c
 extern void         add_File(char *filename, char* dir_name);
-extern void         print_permissions(mode_t mode);
-extern void         setPermissions(char *source, char *dest);
+
+
 //FUNCTIONS from glob2regex
-extern char         *glob2regex(char *glob);
+
 //FUNCTIONS from patterns.c
-extern bool         is_match(char *filename, char *arr[], size_t num_items);
+
 
 extern bool         includeFile(char* fileName); 
-extern bool dir_find(LIST *list, char *wanted);
+
 //Takes each file of each directory,
 // and determines whether it should be included 
 //in the syncing process depending on the flags given
